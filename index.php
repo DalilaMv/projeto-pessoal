a8e9f159de5672f319cce7536a907024db555440
Daniel Vargas <daniel.vargas@sga.pucminas.br>
2018-11-13 09:44:57 -0200

    Inserindo Materialize

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title>
-</head>
-<body>
-   <div>
 
-              
-   </div>
-    
+<head>
+    <title> Famous Love </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Materialize CSS-->
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        /* Fonte */
+        .ralewayFont {
+            font-family: 'Raleway';
+        }
+
+        body {
+            background-color: #f2f2f2;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div>
+
+
+    </div>
+
+    <!-- Jquery-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <!--Materialize JS-->
+    <script src="js/materialize.min.js"></script>
 </body>
+
 </html>
