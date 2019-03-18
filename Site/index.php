1330ab9c4f551d8c8657010e9b454b89ae0858a3
Daniel Vargas <daniel.vargas@sga.pucminas.br>
2019-03-17 22:13:42 -0300

    Index 4.5

--- Site/index.php
+++ Site/index.php
@@ -3,10 +3,11 @@ require_once('common/navbar.php');
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 
 <head>
     <title> Famous Love </title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!-- Logo na Aba do Navegador -->
